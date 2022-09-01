b8cb3e2de95c606adce4cd580a79b4f4506802a5
jonathanOwlting <jonathan_tang@owlting.com>
2022-09-01 16:27:41 +0800

    💡 feat: fetch redmine and copy issue to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 import Box from '@mui/material/Box'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-import ForkRightIcon from '@mui/icons-material/ForkRight';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
+import ForkRightIcon from '@mui/icons-material/ForkRight'
 import KeyIcon from '@mui/icons-material/Key'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+
 import TextField from '@mui/material/TextField'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import FormControl from '@mui/material/FormControl';
+import FormControl from '@mui/material/FormControl'
 import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import { clipboard } from 'electron'
 
 const App: React.FC = () => {
   const [path, setPath] = useState({
     owner: '',
     repo: '',
-    githubToken: ''
+    githubToken: '',
+    redmineToken: '',
+    jiraToken: ''
   })
 
   const [branch, setBranch] = useState({
@@ -23,7 +30,44 @@ const App: React.FC = () => {
     from: 'dev'
   })
 
+  const [jiraCommits, setJiraCommits] = useState([])
+  const [redmineCommits, setredmineCommits] = useState([])
+  const [redmineIssues, setRedmineIssues] = useState([
+    {
+      id: 4951,
+      subject: "PMS銀行資訊api => 改用每日更新於s3 的JSON"
+    }
+  ])
+
+  useEffect(() => {
+    if (redmineCommits.length > 0) {
+      Promise.all(redmineCommits.map(commit => fetchRedmineIssue(getRedmineId(commit))))
+        .then(data => {
+          const excludeStatusList = ['Close', 'On Production']
+          const sortedData = data.sort((a, b) => a.id - b.id).filter(item => !excludeStatusList.some(status => status === item.status))
+          setRedmineIssues(sortedData)
+        })
+    } else {
+      setRedmineIssues([])
+    }
+  }, [redmineCommits])
+
   const option = ['dev', 'release', 'master']
+
+  const jiraPattern = /\[([a-zA-Z\s]+)-\d{4}\]/ // [OW-1234]
+  const matchJiraPatternCommit = (commits:String[]) => {
+    return commits.filter(commit => commit.match(jiraPattern))
+  }
+
+  const redminePattern = /\([a-z:]+ #\d{4}\)/ // done #1234
+  const matchRedminePatternCommit = (commits:String[]) => {
+    return commits.filter(commit => commit.match(redminePattern))
+  }
+  const getRedmineId = (commit:String) => {
+    if (commit.match(redminePattern)) {
+      return commit.match(redminePattern)[0].match(/\d{4}/gm)
+    }
+  }
 
   const handleInput = (value:String, key:String) => {
     const updatePath = {...path}
@@ -49,10 +93,28 @@ const App: React.FC = () => {
       )
       const res = await response.json()
       const list = res.commits.map(item => item.commit.message)
-      console.log(list)
+      setJiraCommits(matchJiraPatternCommit(list))
+      setredmineCommits(matchRedminePatternCommit(list))
     } catch (error) {
-
+      console.error(ErrorEvent)
     }
+  }
+
+  async function fetchRedmineIssue(id) {
+    const url = `https://redmine.owlting.com/issues/${id}.json?key=${path.redmineToken}`
+    try {
+      const response = await fetch(url)
+      const res = await response.json()
+      const { id, subject, status } = res.issue
+      return Promise.resolve({id, subject, status: status.name , markdown: `${subject} [redmine #${id}](https://redmine.owlting.com/issues/${id})`})
+    } catch (error) {
+      console.error(ErrorEvent)
+    }
+  }
+
+  const handleCopyRedmineIssues = () => {
+    const copyContent = redmineIssues.map(issue => issue.markdown).join('\r\n')
+    clipboard.writeText(copyContent)
   }
 
   return (
@@ -109,13 +171,57 @@ const App: React.FC = () => {
         <TextField
           label="github token"
           variant="standard"
+          type="password"
           fullWidth
           onInput={(e) => handleInput(e.target.value as String, 'githubToken')}
           value={path.githubToken}
         />
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'flex-end', m: 2 }}>
+        <KeyIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField
+          label="redmine token"
+          variant="standard"
+          type="password"
+          fullWidth
+          onInput={(e) => handleInput(e.target.value as String, 'redmineToken')}
+          value={path.redmineToken}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', m: 2 }}>
+        <KeyIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField
+          label="jira token"
+          variant="standard"
+          type="password"
+          fullWidth
+          onInput={(e) => handleInput(e.target.value as String, 'jiraToken')}
+          value={path.jiraToken}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end', m: 2 }}>
         <Button variant="contained" onClick={handleFetchBranchDiff} >Fetch!</Button>
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <h1 className='text-xl mb-2'>Jira</h1>
+        {jiraCommits.map(commit => (<p key={commit} className='truncate'>{commit}</p>))}
+      </Box>
+      <Divider light />
+      <Box sx={{ m: 2 }}>
+        <h1 className='text-xl mb-2'>Redmine</h1>
+        {redmineCommits.map(commit => (<p key={commit} className='truncate'>{commit}</p>))}
+      </Box>
+      <Box sx={{ m: 2 }}>
+        <h1 className='text-xl mb-2 mr-2'>
+          Redmine Issue
+          <Button variant="text">
+            <ContentCopyIcon
+              sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+              onClick={handleCopyRedmineIssues}
+            />
+          </Button>
+        </h1>
+        {redmineIssues.map((issue, index) => (<p key={index} className='truncate'>{issue.markdown}</p>))}
       </Box>
     </main>
   )
